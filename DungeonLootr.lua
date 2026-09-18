efc1e59b2d24bc16b941716b3065dc4454aeb393
NickB926 <nickb926@users.noreply.github.com>
2026-09-18 13:43:48 -0700

    Dungeon Lootr 1.0.54: Loop specific Endless presses Continue on floor clear

--- DungeonLootr.lua
+++ DungeonLootr.lua
@@ -93,7 +93,7 @@ Library.ToggleKeybind = { Value = 'Home' }
 Library.Animations = Library.Animations or {}
 Library.Animations.TabSwitch = false
 
-local DL_BUILD = '1.0.53'
+local DL_BUILD = '1.0.54'
 getgenv().DLBuild = DL_BUILD
 
 local Window = Library:CreateWindow({
